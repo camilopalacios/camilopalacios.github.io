eef2b911b0b016325ec17266085ccfa1bebe7bb6
camilopalacios <camilo.palacios.garzon@gmail.com>
2016-05-04 13:24:41 +0200

    Import JQuery and move javascript functions to another file
    Moved the javascript functionality to the application.js file. Now it
    uses JQuery.

---
+++ src/application.js
@@ -0,0 +1,12 @@
+$(document).ready();
+
+
+function showMenu(){
+  $('.topnav').toggleClass('responsive'); // Using javascript: document.getElementsByClassName('topnav')[0].classList.toggle('responsive');
+}
+
+function setActive(){
+  $('.active').removeClass('active'); // Using javascript: document.getElementsByClassName('active')[0].classList.toggle('active');
+  $(document.activeElement).parent().addClass('active'); // Using javascript: document.activeElement.parentElement.classList.toggle('active');
+  showMenu();
+}
