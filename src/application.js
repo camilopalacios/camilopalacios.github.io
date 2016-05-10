6131e6fec250c69e7024e3fa3141c289221e10b6
camilopalacios <camilo.palacios.garzon@gmail.com>
2016-05-10 13:03:34 +0200

    Add multiple obstacles
    The walls (obstacles) are now an array. Now the player may collide on
    various sides, so multi-collisions are handled.

--- src/application.js
+++ src/application.js
@@ -26,7 +26,7 @@ function setActive(){
  */
 $(document).ready(startGame);
 
-//var myGamePiece;
+var myGamePiece, walls, goal;
 
 var myGameArea = {
   canvas: document.createElement('canvas'),
@@ -34,6 +34,7 @@ var myGameArea = {
     this.canvas.width = 480;
     this.canvas.height = 270;
     this.context = this.canvas.getContext('2d');
+    this.keys;
     $('#canvas').append(this.canvas); // javascript: document.getElementById('canvas').insertBefore(this.canvas, document.getElementById('canvas').childNodes[0]);
     this.interval = setInterval(updateGameArea, 20);
     window.addEventListener('keydown', function(event){
@@ -45,12 +46,19 @@ var myGameArea = {
       myGameArea.keys[event.keyCode] = false;
     });
   },
+  restart: function(){
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext('2d');
+    $('#canvas').append(this.canvas); // javascript: document.getElementById('canvas').insertBefore(this.canvas, document.getElementById('canvas').childNodes[0]);
+  },
   stop: function(){
-    myGameArea.context.fillStyle = "white";
-    myGameArea.context.fillRect(30,30, 420, 210);
-    myGameArea.context.fillStyle = "red"; // draw font in red
-    myGameArea.context.font = "40pt sans-serif";
-    myGameArea.context.fillText("Game over", 100, 150);
+
+    this.context.fillStyle = "white";
+    this.context.fillRect(30,30, 420, 210);
+    this.context.fillStyle = "red"; // draw font in red
+    this.context.font = "40pt sans-serif";
+    this.context.fillText("Game over", 100, 150);
     clearInterval(this.interval);
   },
   clear: function(){
@@ -59,10 +67,23 @@ var myGameArea = {
 };
 
 function startGame(){
+  setComponents();
+  myGameArea.start();
+}
+
+function restartGame(){
+  setComponents();
+  myGameArea.restart();
+}
+
+function setComponents(){
   goal = new component(30, 30, 'yellow', 440, 120);
   myGamePiece = new component(30, 30, 'green', 10, 120);
-  myObstacle = new component(10, 200, 'black', 100, 120);
-  myGameArea.start();
+  walls = [];
+  walls.push(new component(100, 10, 'black', 0, 160));
+  walls.push(new component(10, 200, 'black', 100, 120));
+  walls.push(new component(10, 200, 'black', 150, 0));
+  walls.push(new component(250, 10, 'black', 190, 180));
 }
 
 function component(width, height, color, x, y){
@@ -73,9 +94,8 @@ function component(width, height, color, x, y){
   this.speedX = 0;
   this.speedY = 0;
   this.update = function(){
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    myGameArea.context.fillStyle = color;
+    myGameArea.context.fillRect(this.x, this.y, this.width, this.height);
   };
   this.newPos = function(){
     this.x += this.speedX;
@@ -92,48 +112,53 @@ function component(width, height, color, x, y){
     obstacleBottom = obstacle.y + (obstacle.height);
     return ((myBottom < obstacleTop) || (myTop > obstacleBottom) || (myRight < obstacleLeft) || (myLeft > obstacleRight)) ? false:true;
   };
-
-  this.getCollisionSide = function(obstacle){
-    var myLeft = this.x,
-    myRight = this.x + (this.width),
-    myTop = this.y,
-    myBottom = this.y + (this.height),
-    obstacleLeft = obstacle.x,
-    obstacleRight = obstacle.x + (obstacle.width),
-    obstacleTop = obstacle.y,
-    obstacleBottom = obstacle.y + (obstacle.height),
-    overlap = 9999,
-    side = undefined;
-    if(myBottom > obstacleTop && myBottom < obstacleBottom){
-      if(myBottom-obstacleTop < overlap){
-        overlap = myBottom-obstacleTop;
-        side = 'bottom';
+  this.getCollisionSides = function(){
+    var sides = [];
+    for(w in walls){
+      if(myGamePiece.crashWith(walls[w])){
+        var myLeft = this.x,
+        myRight = this.x + (this.width),
+        myTop = this.y,
+        myBottom = this.y + (this.height),
+        obstacleLeft = walls[w].x,
+        obstacleRight = walls[w].x + (walls[w].width),
+        obstacleTop = walls[w].y,
+        obstacleBottom = walls[w].y + (walls[w].height),
+        overlap = 9999,
+        side = undefined;
+        if(myBottom > obstacleTop && myBottom < obstacleBottom){
+          if(myBottom-obstacleTop < overlap){
+            overlap = myBottom-obstacleTop;
+            side = 'bottom';
+          }
+        }
+        if(myTop < obstacleBottom && myTop > obstacleTop){
+          if(obstacleBottom-myTop < overlap){
+            overlap = obstacleBottom-myTop;
+            side = 'top';
+          }
+        }
+        if(myRight > obstacleLeft && myRight < obstacleRight){
+          if(myRight-obstacleLeft < overlap){
+            overlap = myRight-obstacleLeft;
+            side = 'right';
+          }
+        }
+        if(myLeft > obstacleLeft && myLeft < obstacleRight){
+          if(obstacleRight-myLeft < overlap){
+            overlap = obstacleRight-myLeft;
+            side = 'left';
+          }
+        }
+        if(side) sides.push(side);
       }
-    };
-    if(myTop < obstacleBottom && myTop > obstacleTop){
-      if(obstacleBottom-myTop < overlap){
-        overlap = obstacleBottom-myTop;
-        side = 'top';
-      }
-    };
-    if(myRight > obstacleLeft && myRight < obstacleRight){
-      if(myRight-obstacleLeft < overlap){
-        overlap = myRight-obstacleLeft;
-        side = 'right';
-      }
-    };
-    if(myLeft > obstacleLeft && myLeft < obstacleRight){
-      if(obstacleRight-myLeft < overlap){
-        overlap = obstacleRight-myLeft;
-        side = 'left';
-      }
-    };
-    return side;
+    }
+    return sides;
   };
 }
 
 function updateGameArea(){
-  var side;
+  var sides;
   myGameArea.clear();
   stopMove();
 
@@ -141,14 +166,16 @@ function updateGameArea(){
     myGameArea.stop();
   }
   else{
-    if(myGamePiece.crashWith(myObstacle)) side = myGamePiece.getCollisionSide(myObstacle);
-    if(myGameArea.keys && myGameArea.keys[37] && (!side || side!='left') && myGamePiece.x >= 0) {moveLeft()}
-    if(myGameArea.keys && myGameArea.keys[38] && (!side || side!='top') && myGamePiece.y >= 0) {moveUp()}
-    if(myGameArea.keys && myGameArea.keys[39] && (!side || side!='right') && myGamePiece.x + myGamePiece.width <= myGameArea.canvas.width) {moveRight()}
-    if(myGameArea.keys && myGameArea.keys[40] && (!side || side!='bottom') && myGamePiece.y + myGamePiece.height <= myGameArea.canvas.height) {moveDown()}
+    sides = myGamePiece.getCollisionSides();
+    if(myGameArea.keys && myGameArea.keys[37] && (!sides.length || jQuery.inArray('left', sides) == -1) && myGamePiece.x >= 0) {moveLeft();}
+    if(myGameArea.keys && myGameArea.keys[38] && (!sides.length || jQuery.inArray('top', sides) == -1) && myGamePiece.y >= 0) {moveUp();}
+    if(myGameArea.keys && myGameArea.keys[39] && (!sides.length || jQuery.inArray('right', sides) == -1) && myGamePiece.x + myGamePiece.width <= myGameArea.canvas.width) {moveRight();}
+    if(myGameArea.keys && myGameArea.keys[40] && (!sides.length || jQuery.inArray('bottom', sides) == -1) && myGamePiece.y + myGamePiece.height <= myGameArea.canvas.height) {moveDown();}
     myGamePiece.newPos();
     myGamePiece.update();
-    myObstacle.update();
+    for(w in walls){
+      walls[w].update();
+    }
     goal.update();
   }
 
